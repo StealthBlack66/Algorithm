664f6ca63ac4e9d98c90877323c5991f09265164
Sim Yeyeong <130235113+StealthBlack66@users.noreply.github.com>
2026-03-20 13:19:32 +0900

    [Silver IV] Title: 스택, Time: 88 ms, Memory: 39440 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -4,42 +4,19 @@ import sys, queue
 input = sys.stdin.readline
 
 T = int(input())
-my_stack = queue.LifoQueue()
-
-def push(st,item):
-    st.put(item)
-
-def pop(st):
-    if st.qsize() <= 0:
-        return -1
-    else:
-        return st.get()
-
-def size(st):
-    return st.qsize()
-
-def empty(st):
-    if st.qsize() <= 0:
-        return 1
-    else:
-        return 0 
-
-def top(st):
-    if st.qsize() <= 0:
-        return -1
-    else: return st.queue[-1]
+my_stack = []
 
 for i in range(T):
     context = list(input().split())
-    if context[0] not in 'push pop size empty top':
+    if context[0] not in ['push', 'pop', 'size', 'empty', 'top']:
         break 
     if context[0] == 'push':
-        push(my_stack, context[1])
+        my_stack.append(context[1])
     elif context[0] == 'pop':
-        print(pop(my_stack))
+        print(my_stack.pop() if my_stack else -1)
     elif context[0] == 'size':
-        print(size(my_stack))
+        print(len(my_stack))
     elif context[0] == 'empty':
-        print(empty(my_stack))
+        print(0 if my_stack else 1)
     elif context[0] == 'top':
-        print(top(my_stack))
+        print(my_stack[-1] if my_stack else -1)
