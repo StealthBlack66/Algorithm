71acf48a3093dd94f0f0ea093058ec80ea6b02ba
Sim Yeyeong <130235113+StealthBlack66@users.noreply.github.com>
2026-03-23 16:45:20 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,70 +1,15 @@
-from collections import deque
-import sys
+import heapq, sys
 
-class BinaryHeap:
-    def __init__(self):
-        self._heap = []
-    def _perc_up(self,cur):
-        while (cur-1)//2>= 0:
-            parent = (cur-1)//2  
-            if self._heap[cur] < self._heap[parent]:
-                self._heap[cur],self._heap[parent] = (self._heap[parent], self._heap[cur])
-            cur = parent
-
-    def _perc_down(self,cur):
-        while (cur*2+1)<len(self._heap):
-            min_child = self._get_min_child(cur)
-            if self._heap[cur] > self._heap[min_child]:
-                self._heap[cur], self._heap[min_child] = (self._heap[min_child], self._heap[cur])
-            else: return
-            cur = min_child
-
-    def _get_min_child(self,cur):
-        if cur*2+2 > len(self._heap)-1:
-            return cur*2+1
-        if self._heap[cur*2+1] <= self._heap[cur*2+2]:
-            return cur*2+1
-        else:
-            return cur*2+2
-        
-    def heapify(self,not_heap):
-        self._heap = not_heap
-        cur = len(self._heap)//2-1
-        while cur >= 0:
-            self._perc_down(cur)
-            cur = cur-1
-
-    def get_min(self):
-        return self._heap[0]
-
-    def insert(self,item):
-        self._heap.append(item)
-        self._perc_up(len(self._heap)-1)
-
-    def delete(self):
-        self._heap[0],self._heap[-1] = self._heap[-1],self._heap[0]
-        dele = self._heap.pop()
-        self._perc_down(0)
-        return dele
-    
-    def is_empty(self):
-        return not bool(self._heap)
-
-    def __len__(self):
-        return len(self._heap)
-               
-    def __str__(self):
-        return str(self._heap)
+my_heap = []
 
 input = sys.stdin.readline
 N = int(input())
-my_heap = BinaryHeap()
 for _ in range(N):
     x = int(input())
     if x == 0:
-        if my_heap.is_empty():
+        if not my_heap:
             print(0)
         else:
-            print(my_heap.delete())
+            print(heapq.heappop(my_heap))
     else:
-        my_heap.insert(x)
+        heapq.heappush(my_heap,x)
